05dfeb6f2c97d9b2a7af07e41766dd354b903a75
Abdelrhman Kotb <87202131+abdelrhmaKotb@users.noreply.github.com>
2022-12-04 00:58:59 +0200

    Maximum Element

---
+++ hackerrank/Data-Structures/Stack/Maximum-Element.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+#include <string.h>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'getMax' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts STRING_ARRAY operations as parameter.
+ */
+
+vector<int> getMax(vector<string> operations) {
+    vector<int> v;
+    vector<int> s;
+    
+    if(operations.begin() == operations.end())
+    {
+        auto i =   operations.begin();
+        if(stoi(i[0]) == 1)
+        {
+            string temp = (*i).substr(1);
+            v.push_back(stoi(temp));
+        }
+        
+        return v;
+    }
+    
+    for(auto i = operations.begin();i != operations.end();i++)
+    {
+        if(stoi(i[0]) == 1)
+        {
+            string temp = (*i).substr(1);
+            s.push_back(stoi(temp));
+        }else if (stoi(i[0]) == 2)
+        {
+            if(!s.empty())
+            { 
+                s.pop_back();
+            }
+        }else if(stoi(i[0]) == 3) {
+            if(!s.empty())
+            {
+                auto q = s.begin();
+                int max = *q;
+                while (q != s.end()) {
+                    if(max < *q)
+                    {
+                        max = *q;
+                    }
+                    q++;
+                } 
+                v.push_back(max);   
+            }
+            
+            
+        }
+    }
+    return v;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<string> ops(n);
+
+    for (int i = 0; i < n; i++) {
+        string ops_item;
+        getline(cin, ops_item);
+
+        ops[i] = ops_item;
+    }
+
+    vector<int> res = getMax(ops);
+
+    for (size_t i = 0; i < res.size(); i++) {
+        fout << res[i];
+
+        if (i != res.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
