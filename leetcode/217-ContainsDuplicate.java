97a5446aee00a946615cdac760db3bdf185df35a
Abdelrhman Kotb <87202131+abdelrhmaKotb@users.noreply.github.com>
2022-12-07 16:16:08 +0200

    217. Contains Duplicate

---
+++ leetcode/217-ContainsDuplicate.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0,length = nums.length;i < length;i++)
+        {
+            if(map.containsKey(nums[i]))
+            {   
+                return true;
+            }else
+            {
+                map.put(nums[i],nums[i]);
+            }
+        }
+        return false;
+    }
+}
