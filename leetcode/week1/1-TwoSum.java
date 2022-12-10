1eb80f2126d159a992d408486a40a964e3b18909
Abdelrhman Kotb <87202131+abdelrhmaKotb@users.noreply.github.com>
2022-12-10 04:05:59 +0200

    1. Two Sum

---
+++ leetcode/week1/1-TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int[] res = {5,5};
+
+        for(int i = 0,len = nums.length;i < len;i++)
+        {
+            int key = target - nums[i];
+            
+            if(map.containsKey(key))
+            {
+                res[0] = map.get(key);
+                res[1] = i;
+            }else
+            {
+                map.put(nums[i],i);
+            }
+        } 
+
+        return res;
+    }
+}
